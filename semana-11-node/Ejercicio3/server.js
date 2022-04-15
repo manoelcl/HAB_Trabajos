7bd6541eedeeda90f8dbaecae5cbd109c9d7cdbb
Manoel Castro <manoelcl@gmail.com>
2022-04-15 17:25:47 +0200

    Ejercicios semana 11, nodejs

---
+++ semana-11-node/Ejercicio3/server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const fs = require("fs/promises");
+
+const express = require("express");
+const chalk = require("chalk");
+
+const app = express();
+const port = 3000;
+
+app.use((req, res, next) => {
+  console.log(req.path, req.body, req.query);
+  next();
+});
+
+app.get("/current-time", (req, res, next) => {
+  const date = new Date();
+  res.send(`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
+});
+app.get("/force-error", (req, res, next) => {
+  throw new Error("Error de prueba");
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(err.message);
+});
+app.use((req, res, next) => {
+  res.status(404);
+  res.send("NOT FOUND");
+});
+app.listen(port, () => {
+  console.log(chalk.green(`Server started @ port ${port}.`));
+});
